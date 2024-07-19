9385799ad35ca18cd051b77f7bbaabc8e6d8d51c
sherwintayo <sherwintayo08@gmail.com>
2024-07-20 00:25:44 +0800

    update initialize.php

--- initialize.php
+++ initialize.php
@@ -2,7 +2,7 @@
 
 if(!defined('base_url')) define('base_url','http://localhost/bsit_projectrepo/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-if(!defined('DB_SERVER')) define('DB_SERVER',"http://127.0.0.1/");
+if(!defined('DB_SERVER')) define('DB_SERVER',"");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"bsit_repo");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"1Bsit_repo");
 if(!defined('DB_NAME')) define('DB_NAME',"bsit_repo");
