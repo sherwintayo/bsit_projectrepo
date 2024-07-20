ceb4159bacf0ca7c23f0f17513bf1341c29db41f
sherwintayo <sherwintayo08@gmail.com>
2024-07-20 13:32:27 +0800

    edit initialize

--- initialize.php
+++ initialize.php
@@ -2,7 +2,7 @@
 
 if(!defined('base_url')) define('base_url','http://localhost/bsit_projectrepo/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-if(!defined('DB_SERVER')) define('DB_SERVER',"http://127.0.0.1/");
+if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"u510162695_bsit_repo");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"1Bsit_repo");
 if(!defined('DB_NAME')) define('DB_NAME',"u510162695_bsit_repo");
