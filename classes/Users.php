aca96f6a1db9e976707ff1590cdc1c9bd6a5322a
Sherwin Tayo <sherwintayo08@gmail.com>
2024-07-24 00:36:36 +0800

    update forgot pass

--- classes/Users.php
+++ classes/Users.php
@@ -253,60 +253,61 @@ Class Users extends DBConnection {
 	}
 
 	public function forgot_password() {
-        // Extract email from the POST data
-        $email = isset($_POST['email']) ? $_POST['email'] : '';
-
-        // Check if the email is not empty
-        if (empty($email)) {
-            return json_encode(['status' => 'error', 'msg' => 'Email address is required']);
-        }
-
-        // Check if the email exists in the database
-        $qry = $this->conn->query("SELECT * FROM `student_list` WHERE email = '{$email}'");
-        if ($qry->num_rows > 0) {
-            $user = $qry->fetch_assoc();
-            $token = bin2hex(random_bytes(50));
-            $expires = date("U") + 1800; // 30 minutes
-
-            // Delete any existing reset requests for this email
-            $this->conn->query("DELETE FROM `password_resets` WHERE email = '{$email}'");
-            // Insert the new reset request
-            $this->conn->query("INSERT INTO `password_resets` (`email`, `token`, `expires_at`) VALUES ('{$email}', '{$token}', '{$expires}')");
-
-            // Create the reset URL
-            $url = base_url . "reset_password.php?token=" . $token;
-
-            // Send the email using PHPMailer
-            $mail = new PHPMailer(true);
-            try {
-                //Server settings
-                $mail->isSMTP();
-                $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
-                $mail->SMTPAuth = true;
-                $mail->Username = 'your-email@gmail.com'; // Your Gmail address
-                $mail->Password = 'your-email-password'; // Your Gmail password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port = 587;
-
-                //Recipients
-                $mail->setFrom('your-email@gmail.com', 'Mailer');
-                $mail->addAddress($email); // Add a recipient
-
-                // Content
-                $mail->isHTML(true);
-                $mail->Subject = 'Password Reset Request';
-                $mail->Body    = "We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.<br><br>Here is your password reset link:<br><a href='$url'>$url</a>";
-                $mail->AltBody = "We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.\n\nHere is your password reset link:\n$url";
-
-                $mail->send();
-                return json_encode(['status' => 'success']);
-            } catch (Exception $e) {
-                return json_encode(['status' => 'error', 'msg' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
-            }
-        } else {
-            return json_encode(['status' => 'error', 'msg' => 'No account found with that email']);
-        }
-    }
+		// Extract email from the POST data
+		$email = isset($_POST['email']) ? $_POST['email'] : '';
+	
+		// Check if the email is not empty
+		if (empty($email)) {
+			return json_encode(['status' => 'error', 'msg' => 'Email address is required']);
+		}
+	
+		// Check if the email exists in the database
+		$qry = $this->conn->query("SELECT * FROM `student_list` WHERE email = '{$email}'");
+		if ($qry->num_rows > 0) {
+			$user = $qry->fetch_assoc();
+			$token = bin2hex(random_bytes(50));
+			$expires = date("U") + 1800; // 30 minutes
+	
+			// Delete any existing reset requests for this email
+			$this->conn->query("DELETE FROM `password_resets` WHERE email = '{$email}'");
+			// Insert the new reset request
+			$this->conn->query("INSERT INTO `password_resets` (`email`, `token`, `expires_at`) VALUES ('{$email}', '{$token}', '{$expires}')");
+	
+			// Create the reset URL
+			$url = base_url . "reset_password.php?token=" . $token;
+	
+			// Send the email using PHPMailer
+			$mail = new PHPMailer(true);
+			try {
+				//Server settings
+				$mail->isSMTP();
+				$mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+				$mail->SMTPAuth = true;
+				$mail->Username = 'your-email@gmail.com'; // Your Gmail address
+				$mail->Password = 'your-email-password'; // Your Gmail password
+				$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+				$mail->Port = 587;
+	
+				//Recipients
+				$mail->setFrom('your-email@gmail.com', 'Mailer');
+				$mail->addAddress($email); // Add a recipient
+	
+				// Content
+				$mail->isHTML(true);
+				$mail->Subject = 'Password Reset Request';
+				$mail->Body    = "We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.<br><br>Here is your password reset link:<br><a href='$url'>$url</a>";
+				$mail->AltBody = "We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.\n\nHere is your password reset link:\n$url";
+	
+				$mail->send();
+				return json_encode(['status' => 'success']);
+			} catch (Exception $e) {
+				return json_encode(['status' => 'error', 'msg' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
+			}
+		} else {
+			return json_encode(['status' => 'error', 'msg' => 'No account found with that email']);
+		}
+	}
+	
 
 
 	public function reset_password() {
--- reset_password.php
+++ reset_password.php
@@ -1,19 +1,17 @@
 <?php require_once('./config.php') ?>
 <!DOCTYPE html>
-<html lang="en" class="" style="height: auto;">
+<html lang="en">
 <?php require_once('inc/header.php') ?>
-<body class="hold-transition ">
+<body class="hold-transition">
 <script>
-    start_loader()
+    start_loader();
 </script>
 <style>
-    html, body{
-      height:calc(100%) !important;
-      width:calc(100%) !important;
-    }
-    body{
-      background-size:cover;
-      background-repeat:no-repeat;
+    html, body {
+      height: 100%;
+      width: 100%;
+      background-size: cover;
+      background-repeat: no-repeat;
     }
 </style>
 <div class="h-100 d-flex align-items-center w-100" id="reset-password">
@@ -24,12 +22,15 @@
             </div>
             <div class="card-body">
                 <form action="" id="reset-password-form">
-                    <input type="hidden" name="token" value="<?php echo $_GET['token']; ?>">
+                    <input type="hidden" name="token" value="<?php echo htmlspecialchars($_GET['token']); ?>">
                     <div class="form-group">
                         <input type="password" name="password" id="password" placeholder="New Password" class="form-control form-control-border" required>
                     </div>
+                    <div class="form-group">
+                        <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm New Password" class="form-control form-control-border" required>
+                    </div>
                     <div class="form-group text-right">
-                        <button class="btn btn-default bg-black btn-flat">Submit</button>
+                        <button class="btn btn-default bg-black btn-flat" type="submit">Submit</button>
                     </div>
                 </form>
             </div>
@@ -40,20 +41,31 @@
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="dist/js/adminlte.min.js"></script>
 <script>
-  $(document).ready(function(){
+  $(document).ready(function() {
     end_loader();
-    $('#reset-password-form').submit(function(e){
+
+    $('#reset-password-form').submit(function(e) {
         e.preventDefault();
         var _this = $(this);
         $(".pop-msg").remove();
         var el = $("<div>").addClass("alert pop-msg my-2").hide();
+        
+        // Check if passwords match
+        if ($('#password').val() !== $('#confirm_password').val()) {
+            el.text("Passwords do not match.");
+            el.addClass("alert-danger");
+            _this.prepend(el);
+            el.show('slow');
+            return;
+        }
+
         start_loader();
         $.ajax({
             url: "classes/Users.php?f=reset_password",
             method: 'POST',
             data: _this.serialize(),
             dataType: 'json',
-            error: err => {
+            error: function(err) {
                 console.log(err);
                 el.html("An error occurred while processing your request: " + err.responseText);
                 el.addClass("alert-danger");
@@ -62,28 +74,23 @@
                 end_loader();
             },
             success: function(resp) {
-                if (resp.status == 'success') {
+                if (resp.status === 'success') {
                     alert_toast("Password has been reset successfully.", 'success');
                     setTimeout(() => {
                         location.href = 'login.php';
                     }, 2000);
-                } else if (resp.msg) {
-                    el.text(resp.msg);
-                    el.addClass("alert-danger");
-                    _this.prepend(el);
-                    el.show('show');
                 } else {
-                    el.text("An error occurred while processing your request");
+                    el.text(resp.msg || "An error occurred while processing your request");
                     el.addClass("alert-danger");
                     _this.prepend(el);
-                    el.show('show');
+                    el.show('slow');
                 }
                 end_loader();
                 $('html, body').animate({scrollTop: 0}, 'fast');
             }
-        })
-    })
-})
+        });
+    });
+  });
 </script>
 </body>
 </html>
