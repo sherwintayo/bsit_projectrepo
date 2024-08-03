9b7ab58d8f8299d8600efb24254bb98c12dce783
sherwintayo <sherwintayo08@gmail.com>
2024-08-04 00:38:07 +0800

    save

--- admin/system_info/index.php
+++ admin/system_info/index.php
@@ -1,5 +1,7 @@
 <?php if($_settings->chk_flashdata('success')): ?>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
+	
     Swal.fire({
         title: 'Success!',
         text: '<?php echo $_settings->flashdata('success') ?>',
