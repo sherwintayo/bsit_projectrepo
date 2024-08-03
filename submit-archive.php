647cc4c5ebf07f030bf386b1a4646b2b1e669ad7
sherwintayo <sherwintayo08@gmail.com>
2024-08-04 01:00:49 +0800

    submit swal

--- submit-archive.php
+++ submit-archive.php
@@ -189,10 +189,9 @@ if(isset($_GET['id']) && $_GET['id'] > 0){
                      
                         Swal.fire({
                         icon: 'success',
-                        title: 'Login Successful',
+                        title: 'Uploaded Successfully',
                         text: 'You will be redirected shortly.',
-                        showConfirmButton: false,
-                        timer: 800
+                      
                     }).then(() => {
                         location.href= "./?page=view_archive&id="+resp.id
                     })
