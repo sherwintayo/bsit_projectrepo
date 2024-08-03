07c95de560668ac5e68520a522978ee091e914c2
sherwintayo <sherwintayo08@gmail.com>
2024-08-03 19:56:15 +0800

    a

--- login.php
+++ login.php
@@ -79,7 +79,7 @@
                                          
                             <div class="col-lg-12">  
                                 <div class="form-group text-right">
-                                <button><a href="<?php echo base_url ?>">Go Back</a></button>
+                                <button class="text-left"><a href="<?php echo base_url ?>">Go Back</a></button>
                                     <button class="btn btn-default bg-black btn-flat"> Login</button>
                                 </div>
                             </div>
