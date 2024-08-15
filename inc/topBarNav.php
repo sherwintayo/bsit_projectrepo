02726e065d0be00eadedd239c81eba35348431b7
sherwintayo <sherwintayo08@gmail.com>
2024-08-16 00:18:52 +0800

    color

--- inc/topBarNav.php
+++ inc/topBarNav.php
@@ -73,7 +73,7 @@
           padding-top: calc(5em) !important;
       }
       </style>
-<nav class="bg-navy w-100 px-2 py-1 position-fixed top-0" id="login-nav">
+<nav class="bg-danger w-100 px-2 py-1 position-fixed top-0" id="login-nav">
   <div class="d-flex justify-content-between w-100">
     <div>
       <span class="mr-2 text-white"><i class="fa fa-phone mr-1"></i> <?= $_settings->info('contact') ?></span>
