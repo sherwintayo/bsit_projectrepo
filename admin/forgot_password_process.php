17635c1f649d1419b344593cc2723975fda3242d
sherwintayo <sherwintayo08@gmail.com>
2024-09-09 16:12:14 +0800

    .

--- admin/forgot_password_process.php
+++ admin/forgot_password_process.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('../config.php');
 require_once('../vendor/autoload.php'); 
 
